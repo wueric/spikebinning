9ebe4def4448f9ba10069aa88036de433b42ac78
Eric Wu <wueric@stanford.edu>
2022-08-03 13:46:55 -0700

    test

--- spikebinning/spikebinning_cpplib/MergeWrapper.h
+++ spikebinning/spikebinning_cpplib/MergeWrapper.h
@@ -12,30 +12,24 @@ struct MergeWrapper {
     T *base_ptr;
     int64_t dim0;
     int64_t curr_ix;
-    T priority;
 
 
     MergeWrapper<T>(T *ptr_, int64_t dim0_) : base_ptr(ptr_), dim0(dim0_), curr_ix(0) {
-        priority = -(*base_ptr);
     };
 
     MergeWrapper<T>(T *ptr_, int64_t dim0_, int64_t curr_ix_) : base_ptr(ptr_), dim0(dim0_), curr_ix(curr_ix_) {
-        priority = -(*(base_ptr + curr_ix_));
     };
 
-    T getCurrent() const {return -priority;}
+    T priority() {
+        return -(*(base_idx + curr_ix));
+    }
 
-    bool atEnd() const {return curr_ix == dim0;}
+    T getCurrent() const {return *(base_idx + curr_ix);}
+
+    bool atEnd() const {return curr_ix >= dim0;}
 
     void increment() {
-
-        ++curr_ix;
-
-        if (atEnd()) {
-            priority = std::numeric_limits<T>::min();
-        } else {
-            priority = -(*(base_ptr + curr_ix));
-        }
+        if (!atEnd()) ++curr_ix;
     }
 
     friend bool operator< (MergeWrapper<T> const& lhs, MergeWrapper<T> const& rhs) {
@@ -46,7 +40,7 @@ struct MergeWrapper {
 template<class T>
 struct ComparePriority {
     bool operator()(MergeWrapper<T> const & p1, MergeWrapper<T> const & p2) {
-        return p1.priority < p2.priority;
+        return p1.priority() < p2.priority();
     }
 };
 
